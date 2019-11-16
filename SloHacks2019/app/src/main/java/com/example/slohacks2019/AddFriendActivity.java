231a0f1f3acd62ed197b7597795fb0fe2f3521c5
Dylan Mooers <dmooers@calpoly.edu>
2019-11-16 13:59:18 -0800

    Finished addFriendActivity

--- SloHacks2019/app/src/main/java/com/example/slohacks2019/AddFriendActivity.java
+++ SloHacks2019/app/src/main/java/com/example/slohacks2019/AddFriendActivity.java
@@ -1,9 +1,12 @@
 package com.example.slohacks2019;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Map;
 
 public class AddFriendActivity extends Activity {
 
@@ -11,19 +14,32 @@ public class AddFriendActivity extends Activity {
     private TextInputLayout friendNumber;
     private TextInputLayout interestingFact;
 
+    Intent intent = this.getIntent();
+    Bundle bundle = intent.getExtras();
+
+    private User user = (User) bundle.getSerializable("user");
+    private Map<String, User> friendList =
+            (Map<String, User>) bundle.getSerializable("friends");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    protected User newFriend() {
+    protected void newFriend() {
         friendField = findViewById(R.id.friend_field);
         friendNumber = findViewById(R.id.friend_number);
         interestingFact = findViewById((R.id.interesting_fact));
 
+        String name = friendField.getEditText().getText().toString();
+        String number = friendNumber.getEditText().getText().toString();
+        String fact = interestingFact.getEditText().getText().toString();
 
+        user.updateName(name);
+        user.updateFact(fact);
 
+        friendList.put(user.getIdentifier(), user);
     }
 
 }
