31699d9d96bf44bbb81b335fb8b2d92370e9de66
Dylan Mooers <dmooers@calpoly.edu>
2019-11-16 10:44:59 -0800

    Added boiler plate for user class, updated permissions in manifest

---
+++ SloHacks2019/app/src/main/java/com/example/slohacks2019/User.java
@@ -0,0 +1,37 @@
+package com.example.slohacks2019;
+
+public class User {
+
+    private String name;
+    private String identifier;
+    private String imagePath;
+
+    public User(String identifier) {
+        this.identifier = identifier;
+        this.name = "";
+        this.imagePath = "";
+    }
+
+    public User(String identifier, String name) {
+        this.identifier = identifier;
+        this.name = name;
+    }
+
+    public User(String identifier, String name, String imagePath) {
+        this.identifier = identifier;
+        this.name = name;
+        this.imagePath = imagePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+}
